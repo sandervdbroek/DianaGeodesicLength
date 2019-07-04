e2f38c0b0ab8d7d8de3d4ac4406785bb27ff3617
svdb <s.vandenbroek@isd.uni-hannover.de>
2019-07-04 15:05:13 +0200

    Reduced the smallest size

--- Cube.py
+++ Cube.py
@@ -16,7 +16,7 @@ from readOutput import *
 lengthX = 1
 lengthY = 1
 lengthZ = 1
-elementSizes = [0.025, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.5]
+elementSizes = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.5]
 timeSteps = [0.0001, 0.0005, 0.001, 0.0025, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.075, 0.1, 0.125, 0.15, 0.2, 0.3, 0.4, 0.5]
 # timeSteps = [0.04]
 # elementSizes = [0.2]
